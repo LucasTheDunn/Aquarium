7926a682b0c7034a2b34e834c4de4d432ddd58dc
LucasTheDunn <69225625+LucasTheDunn@users.noreply.github.com>
2020-08-26 12:33:16 +1000

    aquarium.js
    eeeee

---
+++ aquarium.js
@@ -0,0 +1,29 @@
+      window.onload=function() {
+                lengthObj = document.getElementById('txtLength');
+                widthObj = document.getElementById('txt');
+                heightObj = document.getElementById('txtHeight');
+                document.getElementById('btnReset').onclick = resetInputs;
+                document.getElementById('btnCalc').onclick = calcCost;
+            }
+            function resetInputs() {
+                lengthObj.value = '';
+                widthObj.value = '';
+                heightObj.innerHTML = '';
+            }
+            function calcTSA() {
+                var length = new Number(lengthObj.value);
+                var width = new Number(widthObj.value);
+                var height = new Number(heightObj.value);
+                return ((length*height*2)+(width*height*2)+(length*width*2) );
+			
+			}
+			function calcGlue 
+				var length = new Number(lengthObj.value);
+                var width = new Number(widthObj.value);
+                var height = new Number(heightObj.value);
+				return ((length*2)+(width*2)+(height*4))
+			}
+			{
+			function calcCost 
+				var TSA = calcTSA();
+			}
