51f39359feff125794dcbf3b393115e526b85fc4
LucasTheDunn <69225625+LucasTheDunn@users.noreply.github.com>
2020-09-02 11:17:53 +1000

    e

--- aquarium.js
+++ aquarium.js
@@ -10,10 +10,17 @@
                 widthObj.value = '';
                 heightObj.innerHTML = '';
             }
-            function calcTSA() {
-                var length = new Number(lengthObj.value);
+            //This is to calculate the total surface area. I also want to send a message to "calcGlue" that glassWidth will be 26 //. 
+			function calcTSA() {
+                var glassWidthmm
+				var glassCost
+				var length = new Number(lengthObj.value);
                 var width = new Number(widthObj.value);
                 var height = new Number(heightObj.value);
+				if (height => 25)
+					make var glassWidthmm = 6
+				else 
+					make var glassWidthmm = 4
                 return ((length*height*2)+(width*height*2)+(length*width*2) );
 			
 			}
@@ -21,9 +28,13 @@
 				var length = new Number(lengthObj.value);
                 var width = new Number(widthObj.value);
                 var height = new Number(heightObj.value);
-				return (((length*2)+(width*2)+(height*4)(*0.10)
+				var glass
+				if (height =>25) 
+					make var glassCost = 
+				return (((length*2)+(width*2)+(height*4))(*0.10))
 			}
 			
 			function calcCost() {
 				var TSA = calcTSA();
+				var 
 			}
